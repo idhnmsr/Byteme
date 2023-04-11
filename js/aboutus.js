1a394796976fedd8c75ddd04c1ebb609e4c785d8
PatriciaLooi <patricia_looi@yahoo.com>
2023-04-11 17:08:28 +0800

    Update About Us

---
+++ js/aboutus.js
@@ -0,0 +1,21 @@
+// /* <script>
+//     function myFunction() {
+//         let dots = document.getElementById("dots");
+
+//     //     let dots = document.getElementById("dots");
+//     //             let moreText = document.getElementById("more");
+//     //     let btnText = document.getElementById("myBtn");
+
+//     //     if (dots.style.display === "none") { */}
+//     //          dots.style.display = "inline";
+//     //          btnText.innerHTML = "Read more";
+//     //          moreText.style.display = "none";
+//     //     }
+//     //     else {
+//     //           dots.style.display = "none";
+//     //          btnText.innerHTML = "Read less";
+//     //         moreText.style.display = "inline";
+
+//     // }
+
+// // </script>
