99db205c4d5d91afead8beddef6cd2a34b4bc477
hadirosman <hadirosman@gmail.com>
2023-04-26 17:08:55 +0800

    Changes to javascript on featured items

--- js/featureditem.js
+++ js/featureditem.js
@@ -1,5 +1,13 @@
 
-const featureditems = [];
+const product1 = {
+  name: "Ram 16Gb",
+  description: "This 16 GB RAM Module",
+  imageURL: "images/ram-16gb.jpg",
+  style: "White / LED",
+  price: 20.50
+}
+
+const featuredItem = [];
 
 function displayProduct() {
 
@@ -8,6 +16,7 @@ function displayProduct() {
     for (let i = 0; i < featuredItem.length; i++) {
 
         display += `
+        <div class="col my-5">
         <div class="card" style="width: 18rem;">
         <img class="py-3" src=${featuredItem[i].imageURL} class="card-img-top" alt="...">
         <div class="card-body">
@@ -15,6 +24,7 @@ function displayProduct() {
         
           <p class="card-text py-3">${featuredItem[i].style}</p>
         </div>
+      </div>
       </div>
     `
     }
@@ -26,7 +36,7 @@ function displayProduct() {
 
 function addProduct(n, d, i, s, p) {
 
-    const featuredItem = {
+    const featureditems = {
         name: n,
         description: d,
         imageURL: i,
@@ -34,10 +44,22 @@ function addProduct(n, d, i, s, p) {
         price: p
     }
 
-    featureditems.push(featuredItem);
+    featuredItem.push(featureditems);
 
 }
 //The individial property valie are sent in thought the argument - later we need to refactor when we have the productform.html
 addProduct("CPU Ryzen 7", "This is a Ryzen 7 CPU", "images/ram-16gb.jpg", "Flower print", 35);
-
 addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+addProduct("CPU Ryzen 9", "This is a Ryzen 9 CPU", "images/ram-16gb.jpg", "Flower print", 35);
+
+
+displayProduct();
